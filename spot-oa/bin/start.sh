634db118d75ab030deebc35641cd51da7ab7b2ff
Diego Ortiz Huerta <diego.ortiz.huerta@intel.com>
2016-12-23 09:20:36 -0800

    Start script now allows users to pass in a custom port

--- spot-oa/bin/start.sh
+++ spot-oa/bin/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # Run ipython web server as a background service
 echo -n "Starting web server... "
-ipython notebook --no-mathjax --profile=ia --port=8889 --ip=0.0.0.0 --no-browser '--NotebookApp.extra_static_paths=["ui/ipython/"]'> ipython.out 2>&1&
+PORT="$1"
+
+if [ "$PORT" = "" ]; then
+    PORT="8889"
+fi
+
+ipython notebook --no-mathjax --profile=ia --port=$PORT --ip=0.0.0.0 --no-browser '--NotebookApp.extra_static_paths=["ui/ipython/"]'> ipython.out 2>&1&
 echo "Done"
