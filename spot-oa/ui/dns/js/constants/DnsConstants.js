08fca223284efbb9be90f81675bc28bb9a38d0c0
Diego Ortiz Huerta <diego.ortiz.huerta@intel.com>
2017-02-22 12:51:08 -0800

    DNS Storyboard migrated to GraphQL

--- spot-oa/ui/dns/js/constants/DnsConstants.js
+++
@@ -1,7 +0,0 @@
-var DnsConstants = {
-  // API URLS
-  API_COMMENTS: '../../data/dns/${date}/threats.csv',
-  API_INCIDENT_PROGRESSION: '../../data/dns/${date}/threat-dendro-${id}.csv'
-};
-
-module.exports = DnsConstants;
